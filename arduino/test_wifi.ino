df382944a891008e4908a95302cf105b11ac68ae
Saul-R <estifjarris@gmail.com>
2019-03-14 14:01:16 +0100

    Sending random data to influxDB

--- arduino/test_wifi.ino
+++ arduino/test_wifi.ino
@@ -3,47 +3,32 @@
 #include <WiFiClient.h>
 #include <WiFiUDP.h>
 
-char ssid[] = "<SSID>";     //  your network SSID (name)
-char pass[] = "<PASSWORD>";    // your network password
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
+const char* ssid     = "SSID";
+const char* password = "PASS";
+
+void setup() {
+ Serial.begin(115200);    
+ WiFi.disconnect(); // Disconnect AP
+ WiFi.mode(WIFI_STA);  
+ WiFi.begin(ssid, password); // Connect to WIFI network
+// Wait for connection
+ while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.println(".");
+ }
+ Serial.print("Connected to ");
+ Serial.println(ssid);
+ Serial.print("IP address: ");
+ Serial.println(WiFi.localIP());
+ randomSeed(1);
+}
 
 // the IP address of your InfluxDB host
 byte host[] = {192, 168, 1, 130};
 // the port that the InfluxDB UDP plugin is listening on
-int port = 8089;
+int port = 8089; 
 long randNumber;
-
 WiFiUDP udp;
-
-void setup() {
-  // initialize serial port
-  Serial.begin(9600);
-
-  // attempt to connect using WPA2 encryption
-  Serial.println("Attempting to connect to WPA network...");
-  status = WiFi.begin(ssid, pass);
-
-  // if unable to connect, halt
-  if ( status != WL_CONNECTED) {
-    Serial.println("Couldn't get a WiFi connection");
-    while(true);
-  }
-  // if the conneciton succeeded, print network info
-  else {
-    Serial.println("Connected to network");
-    // print your WiFi shield's IP address:
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
-
-    // print the received signal strength:
-    long rssi = WiFi.RSSI();
-    Serial.print("signal strength (RSSI):");
-    Serial.print(rssi);
-    Serial.println(" dBm");
-  }
-  randomSeed(1);
-}
 
 float getDistance() {
   float return_distance;
