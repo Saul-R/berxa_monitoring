8ace59faace025dadb286c52e81515652e708675
Saul-R <saulrodriguezit@gmail.com>
2025-06-07 19:44:39 +0200

    Add a small script to retry creation of free VM

--- infrastructure/cloud/terraform/oci-waitscript-create-instance.sh
+++ infrastructure/cloud/terraform/oci-waitscript-create-instance.sh
@@ -1,6 +1,7 @@
 # This scripts just tries to bruteforce the creation of a VM.Standard.E2.1.Micro instance in OCI as you'd normally face a "Out of capacity" error
-while [ ! -f /tmp/test ]; do
+while [ ! -f /tmp/success_creation ]; do
+  echo "Applying terraform"
   terraform apply --auto-approve && touch /tmp/success_creation
   echo "[ERROR] Retrying in 5 minutes"
-  wait 300
+  sleep 300
 done
