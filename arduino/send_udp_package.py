dd8036288549f0a5b6e3ee6d94c4ca31da8a79c9
saulr <saulrodriguezit@gmail.com>
2025-06-23 22:21:58 +0200

    Send udp package to dns, fix Line query

--- arduino/send_udp_package.py
+++ arduino/send_udp_package.py
@@ -1,9 +1,10 @@
 import socket
 import sys
 
-UDP_IP = "35.246.55.79 "
+UDP_IP = "monitoring.piscifactoriaberxa.com"
 UDP_PORT = 8089
-MESSAGE = "distance value=" + sys.argv[1]
+# TODO: Device tag should be auto fetched
+MESSAGE = "sensor_data,device=yun1 distance=" + sys.argv[1]
 
 sock = socket.socket(socket.AF_INET, # Internet
              socket.SOCK_DGRAM) # UDP
